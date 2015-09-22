9f9577c8a4ec43471873eca12e25fa66e09861f4
zhzhang2012 <zhzhangtony@gmail.com>
2015-09-22 01:15:19 -0700

    Add node project bootstrapping

--- bin/nodejs-ci.js
+++ bin/nodejs-ci.js
@@ -9,22 +9,43 @@
  */
 var program = require('commander'),
     shell = require('shelljs'),
+    child_process = require('child_process'),
+    exec = child_process.exec,
     fs = require('fs'),
     clc = require('cli-color'),
     path = require('path');
 
 var createProject = function () {
-    var cloneTemplateChild = shell.exec('git clone https://github.com/zhzhang2012/nodejs-ci.git ~/.grunt-init/nodeci');
-    var child = shell.exec('npm install -g grunt-init && grunt-init nodeci/bootstrap', {async: true});
+    process.stdout.write(clc.cyanBright('Fetching the grunt template for bootstrapping Node project...\n'));
+    shell.exec('git clone https://github.com/zhzhang2012/nodejs-ci.git ~/.grunt-init/nodeci');
+    process.stdout.write('\n');
+
+    process.stdout.write(clc.cyanBright('Installing necessary grunt dependencies...\n'));
+    shell.exec('npm install -g grunt && npm install -g grunt-init');
+    process.stdout.write('\n');
+
+    process.stdout.write(clc.cyanBright('Starting bootstrapping Node project...\n'));
+    var child = exec('grunt-init nodeci', function (error, stdout, stderr) {
+        console.log(stdout);
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        }
+    });
+
+    // CLI read and write event listeners
     child.stdout.on('data', function (data) {
-        console.log(data);
-        //if (code !== 0) {
-        //    console.error(clc.red("Error occurred while installing essential dependencies"));
-        //    console.log(clc.white("You may need administrator access. Try again with sudo [sudo nodeci create]."));
-        //}
-        //
-        //console.log(output);
+        process.stdout.write(data);
+    });
+    child.stdout.on('end', function () {
+        child.stdin.write('\n');
         process.exit(1);
+    });
+    // Read stdin and send to child process
+    process.stdin.on('readable', function () {
+        var chunk = process.stdin.read();
+        if (chunk !== null) {
+            child.stdin.write(chunk);
+        }
     });
 };
 
@@ -53,5 +74,3 @@ program
     .option('docker, --docker', 'Add docker integration', configDocker)
     .option('deploy, --deploy', 'Deploy to cloud container', deploy)
     .parse(process.argv);
-
-
